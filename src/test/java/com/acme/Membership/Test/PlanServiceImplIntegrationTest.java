1f87049e337a39c66f612a3c2c1dbccc95dde762
SebTou08 <sebaas14toulier@gmail.com>
2021-09-16 00:27:51 -0500

    feat: add test to get plan by name

--- src/test/java/com/acme/Membership/Test/PlanServiceImplIntegrationTest.java
+++ src/test/java/com/acme/Membership/Test/PlanServiceImplIntegrationTest.java
@@ -10,6 +10,7 @@ import com.acme.homehealthy.MemberShip.domain.service.PlanService;
 import com.acme.homehealthy.MemberShip.domain.service.SubscriptionService;
 import com.acme.homehealthy.MemberShip.service.PlanServiceImpl;
 import com.acme.homehealthy.MemberShip.service.SubscriptionServiceImpl;
+import com.acme.homehealthy.Social.domain.model.Score;
 import com.acme.homehealthy.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class PlanServiceImplIntegrationTest {
@@ -51,7 +53,7 @@ public class PlanServiceImplIntegrationTest {
         Plan plan = new Plan();
         plan.setId(1L);
         plan.setName("Nivel 1");
-        Mockito.when(planRepository.findPlanByName(plan.getName()))
+        when(planRepository.findPlanByName(plan.getName()))
                 .thenReturn(Optional.of(plan));
         //Act
         Plan plan2 = new Plan();
@@ -72,13 +74,67 @@ public class PlanServiceImplIntegrationTest {
     public void whe111n(){
         //Arrange
         String template= "Resource %s not found for %s with value %s";
-        Mockito.when(planRepository.findById(1L))
+        when(planRepository.findById(1L))
                 .thenReturn(Optional.empty());
         String expectedMessage = String.format(template, "Plan","Id",1L);
         //Act
         Throwable exception = catchThrowable(()-> {
             Plan result = planService.getPlanById(1L);
         });
+        //Assert
+        assertThat(exception)
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage(expectedMessage);
+    }
+
+    @Test
+    @DisplayName("Get Plan by Id with a valid Id then returns True")
+    public void getPlanByIdWithValidIdReturnsTrue(){
+        Long id = 2L;
+        Plan plan = new Plan()
+                .setId(id);
+        when(planRepository.findById(id))
+                .thenReturn(Optional.of(plan));
+        //Act
+        Plan foundPlan = planService.getPlanById(id);
+
+        //Assert
+        assertThat(foundPlan.getId()).isEqualTo(plan.getId());
+    }
+
+    @Test
+    @DisplayName("Get Plan by Name with a valid Name then returns True")
+    public void getPlanByNameWithValidNameReturnsTrue(){
+        Long id = 2L;
+        String name = "planName";
+        Plan plan = new Plan()
+                .setId(id)
+                .setName(name);
+        when(planRepository.findPlanByName(name))
+                .thenReturn(Optional.of(plan));
+        //Act
+        Plan foundPlan = planService.getPlanByName(name);
+
+        //Assert
+        assertThat(foundPlan.getId()).isEqualTo(plan.getId());
+        assertThat(foundPlan.getName()).isEqualTo(plan.getName());
+    }
+
+    @Test
+    @DisplayName("get Plan by Name with invalid Name return Resource Not Found Exception")
+    public void getPlanByNameWithInvalidNameReturnsResourceNotFoundException(){
+        String template ="Resource %s not found for %s with value %s";
+        Long id = 1L;
+        String name = "planName";
+        when(planRepository.findPlanByName(name))
+                .thenReturn(Optional.empty());
+        String expectedMessage = String.format(template, "Plan", "Name", name);
+
+        //Act
+        Throwable exception = catchThrowable(()->{
+            Plan foundPlan = planService.getPlanByName(name);
+        });
+
         //Assert
         assertThat(exception)
                 .isInstanceOf(ResourceNotFoundException.class)
