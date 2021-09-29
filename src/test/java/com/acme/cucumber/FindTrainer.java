b77f3e9060e439dffff1ed3962df4fee370cb97e
SebTou08 <sebaas14toulier@gmail.com>
2021-09-29 16:55:48 -0500

    test: findtrainer works

--- src/test/java/com/acme/cucumber/FindTrainer.java
+++ src/test/java/com/acme/cucumber/FindTrainer.java
@@ -1,6 +1,5 @@
 package com.acme.cucumber;
 
-import com.acme.selenium.WebDriverConfig;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,57 +11,59 @@ import static java.lang.Thread.sleep;
 
 public class FindTrainer  {
 
-    public static WebDriver driver;
-    public static  String API_URL = "https://homehealthy.netlify.app";
-    public static String PATH_DRIVER = System.getProperty("user.dir") + "\\driver\\windows\\chromedriver.exe";
+    WebConfig driver;
 
 
     public FindTrainer(){
-        System.setProperty("webdriver.chrome.driver", WebConfig.PATH_DRIVER);
-        driver = new ChromeDriver();
-        driver.get(API_URL);
+       driver = new WebConfig();
     }
 
     @Given("El usuario HomeHealthy nuevo desee buscar un especialista")
     public void elUsuarioHomeHealthyNuevoDeseeBuscarUnEspecialista() throws InterruptedException {
-        driver.findElement(By.xpath("/html/body/app-root/app-division-page/div/div[2]/div/button[1]")).click();
-        driver.findElement(By.xpath("//*[@id=\"mat-input-0\"]")).sendKeys("rodrities@hotmail.com");
-        driver.findElement(By.xpath("//*[@id=\"mat-input-1\"]")).sendKeys("123456789");
-        driver.findElement(By.xpath("/html/body/app-root/app-login/div/mat-card/form/mat-card-actions/div/button")).click();
+        driver.logIn();
         sleep(1000);
     }
 
     @When("El usuario hace click en Collaborators")
     public void elUsuarioHaceClickEnCollaborators() throws InterruptedException {
         sleep(1000);
-        driver.findElement(By.xpath("/html/body/app-root/app-home/app-user-header/mat-toolbar/mat-toolbar-row/div/a[4]")).click();
+        driver.driver.findElement(By.xpath("/html/body/app-root/app-home/app-user-header/mat-toolbar/mat-toolbar-row/div/a[4]")).click();
     }
 
     @Then("el sistema deberá mostrar un formulario con todos los especialistas actuales de la aplicación.")
-    public void elSistemaDeberáMostrarUnFormularioConTodosLosEspecialistasActualesDeLaAplicación() {
+    public void elSistemaDeberáMostrarUnFormularioConTodosLosEspecialistasActualesDeLaAplicación() throws InterruptedException {
+        sleep(3000);
+        driver.close();
     }
 
     @Given("El usuario HomeHealthy se encuentra en la página de Collaborators")
     public void elUsuarioHomeHealthySeEncuentraEnLaPáginaDeCollaborators() throws InterruptedException {
+        driver.logIn();
         sleep(1000);
-        driver.findElement(By.xpath("/html/body/app-root/app-home/app-user-header/mat-toolbar/mat-toolbar-row/div/a[4]")).click();
+        driver.driver.findElement(By.xpath("/html/body/app-root/app-home/app-user-header/mat-toolbar/mat-toolbar-row/div/a[4]")).click();
     }
 
     @When("El usuario escribe el nombre de manera incorrecta")
     public void elUsuarioEscribeElNombreDeManeraIncorrecta() {
-        driver.findElement(By.xpath("//*[@id=\"mat-input-2\"]")).sendKeys("xxxxx");
+        driver.driver.findElement(By.xpath("//*[@id=\"mat-input-2\"]")).sendKeys("xxxxx");
     }
 
     @Then("el sistema no mostrará ningún dato por la mala digitación")
-    public void elSistemaNoMostraráNingúnDatoPorLaMalaDigitación() {
+    public void elSistemaNoMostraráNingúnDatoPorLaMalaDigitación() throws InterruptedException {
+        sleep(3000);
+        driver.close();
     }
+
+
+
 
     @When("El usuario escribe el nombre de manera correcta")
     public void elUsuarioEscribeElNombreDeManeraCorrecta() {
-        driver.findElement(By.xpath("//*[@id=\"mat-input-2\"]")).sendKeys("trainer");
+        driver.driver.findElement(By.xpath("//*[@id=\"mat-input-2\"]")).sendKeys("trainer");
     }
-
     @Then("el sistema mostrará los resultados que coincidan con los datos ingresados")
-    public void elSistemaMostraráLosResultadosQueCoincidanConLosDatosIngresados() {
+    public void elSistemaMostraráLosResultadosQueCoincidanConLosDatosIngresados() throws InterruptedException {
+        sleep(3000);
+        driver.close();
     }
 }
--- src/test/java/com/acme/cucumber/WebConfig.java
+++ src/test/java/com/acme/cucumber/WebConfig.java
@@ -3,23 +3,34 @@ package com.acme.cucumber;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static java.lang.Thread.sleep;
+
 public class WebConfig {
-    public static WebDriver driver;
-    public static  String API_URL = "https://homehealthy.netlify.app";
+    public  WebDriver driver;
+    public  String API_URL = "https://homehealthy.netlify.app";
     public static String PATH_DRIVER = System.getProperty("user.dir") + "\\driver\\windows\\chromedriver.exe";
 
     public WebConfig() {
-    }
-    public static WebDriver getSimpleDriver() {
         String API_URL = "https://homehealthy.netlify.app";
         String PATH_DRIVER = System.getProperty("user.dir") + "\\driver\\windows\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", WebConfig.PATH_DRIVER);
         driver = new ChromeDriver();
         driver.get(API_URL);
-        return driver;
+    }
+    public void logIn() throws InterruptedException {
+        driver.findElement(By.xpath("/html/body/app-root/app-division-page/div/div[2]/div/button[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"mat-input-0\"]")).sendKeys("rodrities@hotmail.com");
+        driver.findElement(By.xpath("//*[@id=\"mat-input-1\"]")).sendKeys("123456789");
+        driver.findElement(By.xpath("/html/body/app-root/app-login/div/mat-card/form/mat-card-actions/div/button")).click();
+        sleep(1000);
+    }
+
+    public void close(){
+        driver.quit();
     }
 
 }
