35ae4816eec62db07c349dcd6c4e4e28c2d8f4a5
Sebastian Toulier <sebaas14toulier@gmail.com>
2021-11-03 16:21:27 -0500

    refactor: modify pom

--- src/main/java/com/acme/homehealthy/HomehealthyApplication.java
+++ src/main/java/com/acme/homehealthy/HomehealthyApplication.java
@@ -3,6 +3,7 @@ package com.acme.homehealthy;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HomehealthyApplication {
 
     public static void main(String[] args) {
